33f2d80d93b3969697144e9c0752b14c2e6136d2
Dirk Arnez <smalldirkalex@gmail.com>
2026-02-16 13:04:12 +0800

    - ypdate numeric-solve.ts

--- numeric-solve.ts
+++ numeric-solve.ts
@@ -1,5 +1,5 @@
 // solve x > 2 and y < 10 and x + 2y = 7
-// microsoft.github.io/z3guide/programming/Z3%20JavaScript%20Examples/#integer-arithmetic
+// https://microsoft.github.io/z3guide/programming/Z3%20JavaScript%20Examples/#integer-arithmetic
 const x = Z3.Int.const('x');
 const y = Z3.Int.const('y');
 Z3.solve(x.gt(2), y.lt(10), x.add(y.mul(2)).eq(7))
